cfe716d39472ca945912d42c236736ce072930c9
Sachin Khedkar <sachink2@outlook.com>
2017-04-19 19:31:06 +0530

    finding alternate way

---
+++ src/js/controllers/tabsController.js
@@ -0,0 +1,22 @@
+skeleton.controller('TabsCtrl', ['$scope', function ($scope) {
+    $scope.tabs = [{
+            title: 'One',
+            url: 'one.tpl.html',
+        }, {
+            title: 'Two',
+            url: 'two.tpl.html',
+        }, {
+            title: 'Three',
+            url: 'three.tpl.html',
+    }];
+
+    $scope.currentTab = 'one.tpl.html';
+
+    $scope.onClickTab = function (tab) {
+        $scope.currentTab = tab.url;
+    };
+    
+    $scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    };
+}]);
