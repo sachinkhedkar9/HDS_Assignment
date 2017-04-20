1b775cbed1b118a118b4920ea3758e35ded126a1
Sachin Khedkar <sachink2@outlook.com>
2017-04-20 11:00:38 +0530

    added data to tabs

--- src/app.js
+++ src/app.js
@@ -10,51 +10,17 @@ skeleton.config(['$stateProvider', '$urlRouterProvider', function($stateProvider
     url: '/hds/',
     templateUrl: 'views/hds/index.html'
   }).state('hds.accounts',{
-    url: '/accounts/',
+    url: 'accounts',
     templateUrl:'views/hds/accounts.html',
     controller: 'accountsController'
   }).state('hds.connections',{
-    url: '/connections',
+    url: 'connections',
     templateUrl:'views/hds/connections.html',
     controller: 'connectionsController'
   });
 
-  $urlRouterProvider.otherwise('/login/');
+  $urlRouterProvider.otherwise('/login/')
+                    .when('/hds/', '/hds/accounts');
 }]);
 
 
-
-
-/**
- * 
-var skeleton = angular.module("skeletonApp",['ui.router']);
-
-skeleton.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-  $stateProvider
-  .state('login',{
-    url: '/login/',
-    templateUrl: 'views/essential/login.html',
-    controller: 'loginController'
-  }).state('hds',{
-    url: '/hds/',
-    templateUrl: 'views/hds/index.html',
-    views:{
-      'accounts':{
-        url: '/accounts/',
-        templateUrl:'views/hds/accounts.html',
-        controller: 'accountsController'
-      },
-      'connections':{
-        url: '/connections',
-        templateUrl:'views/hds/connections.html',
-        controller: 'connectionsController'
-      }
-    }
-  });
-
-  $urlRouterProvider.otherwise('/login/');
-}]);
-
-
-
- */
